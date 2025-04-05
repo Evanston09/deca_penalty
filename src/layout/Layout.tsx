bdb4f961ec5d07a8ef30b743ee96d381bcceb17f
Evanston09 <evanhkim09@gmail.com>
2025-04-05 16:21:50 -0400

    Remove DECA logos

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -1,14 +1,10 @@
-import { Outlet, Link } from "react-router";
+import { Outlet } from "react-router";
 import Footer from "../components/Footer.tsx";
-import logo from "../assets/DECA Logo.png";
 
 function Layout() {
   return (
     <>
       <div className="min-h-screen text-white bg-gray-950 flex flex-col">
-        <Link to="/">
-          <img src={logo} alt="Deca Logo" className="w-48 m-4" />
-        </Link>
         <main className="p-4 grow flex flex-col items-center justify-center">
           <Outlet />
         </main>
